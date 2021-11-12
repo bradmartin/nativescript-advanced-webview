102966ba2b8d12d54275eb62ceeb696aef5b0fc8
Brad Martin <bradwaynemartin@gmail.com>
2021-11-11 23:56:47 -0600

    fix publish script

--- src/scripts/build-native.js
+++ src/scripts/build-native.js
@@ -8,17 +8,17 @@ exec('tns --version', (err, stdout, stderr) => {
     return;
   }
 
-  const tnsVersion = semver.major(stdout);
+  // const tnsVersion = semver.major(stdout);
 
   // execute 'tns plugin build' for {N} version > 4. This command builds .aar in platforms/android folder.
-  if (tnsVersion >= 4) {
-    console.log(`executing 'tns plugin build'`);
-    exec('tns plugin build', (err, stdout, stderr) => {
-      if (err) {
-        // node couldn't execute the command
-        console.log(`${err}`);
-        return;
-      }
-    });
-  }
+  // if (tnsVersion >= 4) {
+  console.log(`executing 'tns plugin build'`);
+  exec('tns plugin build', (err, stdout, stderr) => {
+    if (err) {
+      // node couldn't execute the command
+      console.log(`${err}`);
+      return;
+    }
+  });
+  // }
 });
